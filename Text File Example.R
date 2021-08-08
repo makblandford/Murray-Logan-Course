ceae794fa96be04f92f4fc640676261b7750a32a
Makeely Blandford <makeely.blandford@my.jcu.edu.au>
2021-08-09 09:44:08 +1000

    Moved summary to different text script

--- Text File Example.R
+++ Text File Example.R
@@ -6,3 +6,6 @@ summary(x)
 ##committ gitignore so that it knows what to ignore, even when collaborating.
 ##by convention, first commit message is "initial commit"
 ##main and master branch are the same thing, we are just moving away from calling it "master" 
+
+##you must submit a commit message or you will be taken to vi editor where you wont be able to type. to get out, type ":wq"
+
