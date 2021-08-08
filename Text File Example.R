607cb0c54250c8808855d546082882641cfd6c73
Makeely Blandford <makeely.blandford@my.jcu.edu.au>
2021-08-09 09:41:23 +1000

    Added a summary

--- Text File Example.R
+++ Text File Example.R
@@ -1,3 +1,8 @@
 x=seq(1, 10, len=1)
 y=40*2 + rnorm(10,0,5)
 plot(x,y)
+summary(x)
+
+##committ gitignore so that it knows what to ignore, even when collaborating.
+##by convention, first commit message is "initial commit"
+##main and master branch are the same thing, we are just moving away from calling it "master" 
