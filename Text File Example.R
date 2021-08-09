5344fea07cc3593a2d10d87e92e3c2a86f42f8a4
Makeely Blandford <makeely.blandford@my.jcu.edu.au>
2021-08-09 10:15:29 +1000

    Alternative analysis

--- Text File Example.R
+++ Text File Example.R
@@ -6,3 +6,6 @@ summary(x)
 ##committ gitignore so that it knows what to ignore, even when collaborating.
 ##by convention, first commit message is "initial commit"
 ##main and master branch are the same thing, we are just moving away from calling it "master" 
+
+median(x)
+##new branch has mean, master has median
